0c7af84b247ad85f1dc36090dc614602abd661d6
MesfinGithinji <meshgithinji@gmail.com>
2023-06-07 01:16:34 +0300

    Test Driven Development

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Module defines a function that multiplies two matrices"""
+
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """
+    Return the multiplication of two matrices.
+  
+    """
+
+    return (np.matmul(m_a, m_b))
