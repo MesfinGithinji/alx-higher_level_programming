fecef889d4ee48223064e8ff9a7b7a4d5fc69634
MesfinGithinji <meshgithinji@gmail.com>
2023-06-19 22:43:42 +0300

    Test Cases Done

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,10 +24,10 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """Returns the JSON string representation of list_dictionaries"""
-        if list_dictionaries == None or list_dictionaries == []:
+        if list_dictionaries is None:
             return "[]"
         if (type(list_dictionaries) != list or not
-            all(type(i) == dict for i in list_dictionaries)):
+                all(type(i) == dict for i in list_dictionaries)):
             raise TypeError("list_dictionaries must be a list of dictionaries")
 
         return json.dumps(list_dictionaries)
