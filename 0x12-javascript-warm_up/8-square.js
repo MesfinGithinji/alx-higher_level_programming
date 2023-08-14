0652d0ee8b6e3a4f64091ca08d581b55aa1d6a8b
MesfinGithinji <meshgithinji@gmail.com>
2023-08-14 19:49:41 +0300

    Square pattern based on argument provided

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,15 +2,16 @@
 
 const args = process.argv;
 const newArgs = parseInt(args[2]);
+let pattern = '';
 
 if (newArgs !== Number && isNaN(newArgs)) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < newArgs; i++) {
-    let pattern = '';
     for (let x = 0; x < newArgs; x++) {
       pattern += 'x';
     }
-    console.log(pattern);
+    pattern += '\n';
   }
 }
+console.log(pattern);
