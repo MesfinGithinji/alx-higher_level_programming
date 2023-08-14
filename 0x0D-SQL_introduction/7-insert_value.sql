165f99983f48c6e024722133550965f27447bf93
MesfinGithinji <meshgithinji@gmail.com>
2023-08-14 16:49:37 +0300

    Sql Query

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -3,4 +3,4 @@
 --      id = 89
 --      name = Best School
 
-INSERT INTO `first_table` (`id`, `name`) VALUES(89, 'Best School');
+INSERT INTO `first_table` (`id`, `name`) VALUES(89, 'Best School'); 
