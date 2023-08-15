de45a36f6070edbe0c4a10ce77f42c2f0c6344bf
MesfinGithinji <meshgithinji@gmail.com>
2023-08-16 02:03:06 +0300

    Type check the attributes

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/**
+ * type check parameters and then create empty object
+ */
+class Rectangle {
+  constructor (w, h) {
+    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+module.exports = Rectangle;
