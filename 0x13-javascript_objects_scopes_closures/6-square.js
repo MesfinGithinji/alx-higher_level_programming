e0b4dcc12a9eef8b4bba8185e0ecb84ce8b2f735
MesfinGithinji <meshgithinji@gmail.com>
2023-08-16 02:26:41 +0300

    Add an instance method to class square that prints the rectangle

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+/**
+ * added an instance method charPrint(c)
+ * prints the rectangle using the character c
+ */
+const PrevSquare = require('./5-square');
+
+class Square extends PrevSquare {
+  charPrint (c) {
+    const myChar = c === undefined ? 'X' : c;
+    for (let i = 0; i < this.height; i++) {
+      let myVar = '';
+      let y = 0;
+      while (y < this.width) {
+        myVar += myChar;
+        y++;
+      }
+
+      console.log(myVar);
+    }
+  }
+}
+
+module.exports = Square;
