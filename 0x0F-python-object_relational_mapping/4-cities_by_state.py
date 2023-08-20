bd7af1c71caa35a84795c3b3f44eb28a9a7c1a3d
MesfinGithinji <meshgithinji@gmail.com>
2023-08-20 21:18:19 +0300

    script that lists all cities from the database hbtn_0e_4_usa

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+script that lists all cities from the database hbtn_0e_4_usa
+"""
+
+if __name__ == "__main__":
+
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(
+        host='localhost',
+        user=argv[1],
+        password=argv[2],
+        db=argv[3],
+        port=3306
+    )
+
+    cur = db.cursor()
+
+    cur.execute(
+        """SELECT cities.id, cities.name, states.name
+        FROM cities
+        JOIN states
+        ON states.id = cities.state_id
+        ORDER BY cities.id"""
+    )
+
+    my_data = cur.fetchall()
+
+    for row in my_data:
+        print(row)
+
+    cur.close()
+
+    db.close()
