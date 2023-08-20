0255637cc684aa19ec3ab216156e289438b27712
MesfinGithinji <meshgithinji@gmail.com>
2023-08-20 20:49:03 +0300

    Use of MySQLdb to link to database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     import MySQLdb
     from sys import argv
 
-    db = MySQLdb.connect(host='localhost', user=argv[1], password=argv[2],
-                            db=argv[3], port=3306)
+    db = MySQLdb.connect(host='localhost', user=argv[1],
+                         password=argv[2], db=argv[3], port=3306)
 
     cur = db.cursor()
 
