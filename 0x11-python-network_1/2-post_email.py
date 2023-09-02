efb6707dd3982d71e25123421cc4e0b0d29ad862
MesfinGithinji <meshgithinji@gmail.com>
2023-09-02 03:13:08 +0300

    POST request with email as parameter

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,7 +10,7 @@ import urllib.request
 if __name__ == "__main__":
     url = sys.argv[1]
     Key_value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(key_value).encode("ascii")
+    data = urllib.parse.urlencode(Key_value).encode("ascii")
 
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
