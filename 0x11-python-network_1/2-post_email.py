039c5957839ea50db47829884a0315130e35e94d
MesfinGithinji <meshgithinji@gmail.com>
2023-09-02 03:10:08 +0300

    POST request with email as parameter

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Sends a POST request to the passed URL with email as a parameter
+   Displays the body of the response.
+"""
+import sys
+import urllib.parse
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    Key_value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(key_value).encode("ascii")
+
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode("utf-8"))
