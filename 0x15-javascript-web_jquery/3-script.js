a76fe4070d0d059358ba40fb069afac8d18ea5de
MesfinGithinji <meshgithinji@gmail.com>
2023-10-05 00:59:50 +0300

    Handle onclick event , change header to red when div in clicked and add a class

--- 0x15-javascript-web_jquery/3-script.js
+++ 0x15-javascript-web_jquery/3-script.js
@@ -1,6 +1,6 @@
 /**
  * script that adds the class red to the <header> element when the user clicks on the tag DIV#red_header
- * .addClass will add onclick
+ * .addClass will add onclick and also changes color
  */
 $(document).ready(function () {
     $('div#red_header').click(function () {
