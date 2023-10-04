2094ede18216ec501a6029f1001f3dd36dd26cbc
MesfinGithinji <meshgithinji@gmail.com>
2023-10-05 00:31:30 +0300

    Use Jquery API

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,2 +1,4 @@
 // updates the text color of the <header> element to red (#FF0000):
-document.querySelector('HEADER').style.color = '#FF0000';
+const header = document.querySelector('header');
+
+header.style.color = '#FF0000';
